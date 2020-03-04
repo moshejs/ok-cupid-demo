8158e8312a166eab1042a385196cde7e7e451138
Moshe <moshemalka@mail.adelphi.edu>
2020-03-04 16:01:29 -0500

    quick-deploy

---
+++ docs/precache-manifest.6099ea2d03867b8f7ae27f3467d4623e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b90046d5e4a577bac2b98a29581c061",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8969e54229bd859aeed6",
+    "url": "/static/css/main.83d1670b.chunk.css"
+  },
+  {
+    "revision": "e793c311c3641bcaeec0",
+    "url": "/static/js/2.529d156c.chunk.js"
+  },
+  {
+    "revision": "8969e54229bd859aeed6",
+    "url": "/static/js/main.f0ad888c.chunk.js"
+  },
+  {
+    "revision": "87ccb937659a8a4c960b",
+    "url": "/static/js/runtime~main.cbf31e94.js"
+  }
+]);
